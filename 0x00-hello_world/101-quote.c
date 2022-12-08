ebd1341058e560727f81bd79ac63cc0bd80d9f98
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-08 15:05:05 -0800

    wow

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	write|2, "and that place of art is usful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
