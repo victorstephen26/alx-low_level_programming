375da46463c332efd3cfd3416976d693bcf013dd
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-11 04:30:07 -0800

    wow

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,25 +4,20 @@
 /**
  * mian - Entry point
  *
- * Description: this is my first c program
- *
- * Return: Always 0 (success)
+ * Return: Always return 0 (Success)
  */
 
 int main(void)
 {
 	int n;
-	{
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	printf("%d is positive\n", n);
-	}
+		printf("%d is positive\n", n);
 	else if (n == 0)
-	{
-	printf("%d is zero\n", n);
-	}
+		printf("%d is zero\n", n);
 	else
-	{
-	printf("%d is negative\n", n);
-	}
+		printf("%d is negative\n", n);
 	return (0);
 }
