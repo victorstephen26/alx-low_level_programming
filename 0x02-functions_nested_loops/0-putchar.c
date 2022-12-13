813257d1d38179fb81398ebcf8872de8ac4741dc
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-13 00:52:46 -0800

    wow

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by bwave
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int _putchar;
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	}
+
+	return (0);
+}
