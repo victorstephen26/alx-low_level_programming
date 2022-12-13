0b3aa5f80549dc1b55d1111314c63af0d4e4eb66
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-13 14:07:41 -0800

    wow

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - compute the abs value of an intiger
+ * @n: the int question
+ *
+ * Return: the abs of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
