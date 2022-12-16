4ffa15fd466759762f67bf2f72a64455f00bf583
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-15 21:35:57 -0800

    wow

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,1 @@
-#include "main.h"
-/**
- * print_numbers - print 0123456789
- * owned by ALX
- * Return: void
- */
 
-void print_numbers(void)
-{
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
-}
