c2011cff956fa40fb1593886c1d6ebeb5f7af7f9
victorstephen26 <victorstephenikechukwu24@gmail.com>
2022-12-16 09:24:41 -0800

    wow

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints
- * owned by Bwave ICT
+ * print_most_numbers - print num except 2 and 4
  * Return: void
  */
 
@@ -13,7 +12,7 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (!(c == '2' || c == '4'))
-		_putchar(c);
+			_putchar(c);
 	}
 	_putchar('\n');
 }
