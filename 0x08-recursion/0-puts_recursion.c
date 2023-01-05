e38b5ff89a5d804214742ceacf79e1856b31b648
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-01-05 12:12:17 -0800

    wow

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
  * _puts_recursion - print a string, followed by a new line.
  * @s:string
  * Return:void
+ * On error, -1 is returned, and error is set appropriately.
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == 0)
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
