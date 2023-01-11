4575adbb952eda1a2b480a7f774ce4a400f05f15
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-01-11 04:25:02 -0800

    wow

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of program
+ * @argc: arrat size
+ * @argv: array containing elements
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	puts(argv[argc - 1]);
+
+	return (0);
+}
