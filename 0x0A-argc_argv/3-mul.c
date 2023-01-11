ae0cbdc62eaf46716151b777f8e3ef71db35702a
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-01-11 05:20:49 -0800

    wow

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - multiply 2 numbers
+ * @argc: count
+ * @argv: vector
+ * Return: 0 successful
+ */
+int main(int argc, char **argv)
+{
+	int arr_len = argc - 1;
+
+	if (arr_len != 2)
+	{
+		puts("Error");
+
+		return (1);
+	}
+
+	printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+
+	return (0);
+}
