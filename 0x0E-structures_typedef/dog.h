d80e906c0a50e9c0eede8032977597c07a9ee8c8
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-01-17 02:57:42 -0800

    wow

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,4 +13,11 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void intit_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
