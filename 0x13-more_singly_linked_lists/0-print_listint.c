470f11924f69314ccca2aab880b6dde625b9b81c
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-01-31 06:24:24 -0800

    wow

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,18 +3,18 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of types listint_t to print
- * Return: number of nodes
+ * Return: Number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t num_nodes = 0;
 
 	while (h != NULL)
 	{
+		num_nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		num++;
 	}
 
-	return (num);
+	return (num_nodes);
 }
